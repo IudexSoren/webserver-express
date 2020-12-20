422a6bf9d86a684b2592179ec1f26ddf5b7e95e8
IudexSoren <aaronsibajam@gmail.com>
2020-12-20 12:44:04 -0600

    Commit inicial

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3100;
+
+app.use(express.static(`${ __dirname }/public`));
+// HBS engine
+hbs.registerPartials(`${ __dirname }/views/partials`, (err) => {});
+app.set('view engine', 'hbs');
+
+
+
+app.get('/',  (req, res) => {
+  res.render('home', {
+    nombre: 'Aarón'
+  });
+});
+
+app.get('/about',  (req, res) => {
+  res.render('about');
+});
+
+
+app.listen(port, () => {
+  console.log(`Escuchando el puerto ${port}`);
+});
